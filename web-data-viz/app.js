49f16946c9ce90d230545a26ad7feb08c2728062
Vicios579 <Vinicius.guedes@sptech.school>
2025-11-25 22:25:28 -0300

    Web-Data-Viz Limpo

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -17,9 +17,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var estadoRouter = require("./src/routes/estado");
 var implantesRouter = require("./src/routes/implantes");
-var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,9 +27,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/estado", estadoRouter);
 app.use("/implantes", implantesRouter);
-app.use("/empresas", empresasRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
